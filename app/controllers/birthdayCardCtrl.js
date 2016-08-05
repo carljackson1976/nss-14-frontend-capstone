894badc30722fe72a69d9907f24cd18d09867819
Carl Crawley <carljackson1976@gmail.com>
2016-08-04 20:27:17 -0500

    8/4 four

--- app/controllers/birthdayCardCtrl.js
+++ app/controllers/birthdayCardCtrl.js
@@ -1,3 +1,28 @@
+app.controller("birthdayCardCtrl", function($scope, $location, $http, firebaseURL){
+
+	$scope.cardName = null;
+	$scope.hideElements = false;
+
+
+	$scope.cardCreated = function() {
+
+		$scope.hideElements = true;
+
+		let cardObj = {name: $scope.cardName}
+		cardObj = JSON.stringify(cardObj);
+		let queryString = `${firebaseURL}/cards.json?`;
+		$http.post(queryString, cardObj)
+		.success(function(id) {
+			console.log("Your card was put at id", id);
+		});
+
+	}
+
+
+
+});
+
+
 // app.controller("birthdayCardCtrl", function($scope, $location){
 //   $(document).ready(function(){
 //       $('.collapsible').collapsible({
